be80002883b1ec6941637525735c04ba7160a20b
Siddharth Chauhan <siddharth.chauhan2895@gmail.com>
2018-08-10 14:47:35 +0530

    commit2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+// import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes = Hero[];
   selectedHero: Hero;
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,9 +1,9 @@
 import { Hero } from './hero';
 export const HEROES: Hero[] = [
-    {id: 11, name: 'Mr. Mukul'},
+    {id: 11, name: 'Sai Teja'},
     {id: 12, name: 'Mr. Mamidi'},
     {id: 13, name: 'Perfect Coder'},
     {id: 21, name: 'Rajawat'},
-    {id: 15, name: 'Sai Teja'},
+    {id: 15, name: 'Mr. Mukul'},
     {id: 16, name: 'Hari Krishna'}
 ];
